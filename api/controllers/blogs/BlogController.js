9a5733201acf1926a5b177b0494adc23b0ac4c80
Swap76 <swapnil.satish17@siesgst.ac.in>
2019-04-22 02:27:49 +0530

    Blog Schema and Blogs routes Added

--- api/controllers/blogs/BlogController.js
+++ api/controllers/blogs/BlogController.js
@@ -3,6 +3,30 @@ const debug = require('debug')('api:BlogController');
 const Blog = require('../../models/Blogs');
 const validator = require('validator');
 
-exports.blogview = async (req, res) => {
-    res.send("Blogs")
-}
+module.exports.blogview = async (req, res) => {
+	res.send("Blogs");
+};
+
+module.exports.all = async (req, res) => {
+	res.send("All Blogs");
+};
+
+module.exports.show = async (req, res) => {
+	res.send("Show");
+};
+
+module.exports.dashboard = async (req, res) => {
+	res.send("Dashboard");
+};
+
+module.exports.create = async (req, res) => {
+	res.send("Create");
+};
+
+module.exports.edit = async (req, res) => {
+	res.send("Edit");
+};
+
+module.exports.delete = async (req, res) => {
+	res.send("Delete");
+};
--- api/routes/blog.js
+++ api/routes/blog.js
@@ -7,4 +7,16 @@ const blogController = require('../controllers/blogs/BlogController');
 
 router.get('/0', authController.checkUser, blogController.blogview);
 
+router.get('/', blogController.all);
+
+router.get('/:id', blogController.show);
+
+router.get('/dashboared', authController.checkUser, blogController.dashboard);
+
+router.get('/create', authController.checkUser, blogController.create);
+
+router.get('/:id/edit', authController.checkUser, blogController.edit);
+
+router.get('/:id/delete', authController.checkUser, blogController.delete);
+
 module.exports = router;
