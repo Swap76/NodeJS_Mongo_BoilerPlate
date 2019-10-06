557f362c08688cd939db6a4653f7a4c4d1af1814
sowmiya <Sowmiyamuthuraman@gmail.com>
2019-10-06 21:17:46 +0530

    Update authcontroller to use async/await

--- src/controllers/auth/AuthController.js
+++ src/controllers/auth/AuthController.js
@@ -11,7 +11,7 @@ const validator = require('validator');
  * @method POST
  */
 exports.register = async (req, res) => {
-	const { username, email, password, password2} = req.body;
+	const { username, email, password, password2 } = req.body;
 	const user = {
 		username,
 		email,
@@ -25,42 +25,41 @@ exports.register = async (req, res) => {
 		password2: Joi.string().required().min(8).max(32)
 	};
 	const { error } = Joi.validate(user, check);
-	if (error){
-		res.status(400).send({'error':error.details[0].message});  	
+	if (error) {
+		res.status(400).send({ 'error': error.details[0].message });
 	} else if (password != password2) {
-		res.status(400).send({'error':'Password doesn\'t match'});    	
+		res.status(400).send({ 'error': 'Password doesn\'t match' });
 	} else if (validator.contains(username, ' ')) {
-		res.status(400).send({'error':'"username" should not contain blank space'});   	 
+		res.status(400).send({ 'error': '"username" should not contain blank space' });
 	} else {
 		// If validation Passed
-		User.findOne({ email: email})
-			.then(user => {
-				if(user) {
-    			res.status(400).send({'error':'Email id already registered'});   
-				} else {
-					const newUser = new User({
-						username,
-						email,
-						password
-					});
-					// Hash Password
-					bcrypt.genSalt(10, (err, salt) => 
-						bcrypt.hash(newUser.password, salt, (err, hash) => {
-							if(err) {
-    						res.status(400).send({'error':'Bcrypt Error'});
-							} else {
-								// Set password to hashed
-								newUser.password = hash;
-								// Save user
-								newUser.save()
-									.then(user => {
-										res.status(400).send('You are now registered');
-									})
-									.catch(err => debug(err));
-							}
-					}))
+		const user = await User.findOne({ email: email })
+		if (user) {
+			return res.status(400).send({ 'error': 'Email id already registered' });
+		}
+		const newUser = new User({
+			username,
+			email,
+			password
+		});
+		// Hash Password
+		bcrypt.genSalt(10, (err, salt) =>
+			bcrypt.hash(newUser.password, salt, async (err, hash) => {
+				if (err) {
+					return res.status(400).send({ 'error': 'Bcrypt Error' });
 				}
-			});
+				// Set password to hashed
+				newUser.password = hash;
+				// Save user
+				try {
+					await newUser.save();
+					return res.status(400).send('You are now registered');
+				}
+				catch (err) {
+					debug(err);
+				}
+			}
+			));
 	}
 }
 
@@ -74,38 +73,36 @@ module.exports.login = async (req, res) => {
 	const data = {
 		email: req.body.email,
 		password: req.body.password,
-	  };
-	  const check = {
+	};
+	const check = {
 		email: Joi.string().email().required().trim(),
 		password: Joi.string().required().min(8).max(32)
-	  };
-	  const { error } = Joi.validate(data, check);
-	  if (error) {
-			res.status(400).send({'error':error.details[0].message});	 
-	  }
-	  else {
-		User.findOne({ email: req.body.email}, (err, user) => {
-			if (err) {
-			  debug(err);
-				res.status(400).send({'error':'Some error from mongodb. Try again'});  
-			} else if (user) {
-			  bcrypt.compare(req.body.password, user.password, (err, match) => {
-				if (err) {
-				  debug(err);
-					res.status(400).send({'error':'Some error from bcrypt. Try again'});  
-				} else if (match) {
-					req.session.loggedIn = true;
-					req.session.user = user;
-					res.status(200).send('Logged in.');
-				} else {
-					res.status(400).send({'error':'Incorrect Password'});  
+	};
+	const { error } = Joi.validate(data, check);
+	if (error) {
+		res.status(400).send({ 'error': error.details[0].message });
+	}
+	else {
+		try {
+			const user = await User.findOne({ email: req.body.email });
+			if (user) {
+				try {
+					const match = await bcrypt.compare(req.body.password, user.password);
+					if (match) {
+						req.session.loggedIn = true;
+						req.session.user = user;
+						return res.status(200).send('Logged in.');
+					}
+					return res.status(400).send({ 'error': 'Incorrect Password' });
+				} catch (err) {
+					debug(err);
+					return res.status(400).send({ 'error': 'Some error from mongodb. Try again' });
 				}
-			  });
-			} else {
-				res.status(400).send({'error':'Incorrect email address'});
 			}
-		  });
-	  }
+		} catch (err) {
+			res.status(400).send({ 'error': 'Incorrect email address' });
+		}
+	};
 }
 
 /**
@@ -115,12 +112,12 @@ module.exports.login = async (req, res) => {
  * @method GET
  */
 module.exports.logout = (req, res, next) => {
-  if (req.session.user != null) {
-    req.session.user = null;
-    req.session.loggedIn = false;
+	if (req.session.user != null) {
+		req.session.user = null;
+		req.session.loggedIn = false;
 		res.status(200).send('Logged out');
 	} else {
-		res.status(400).send({'error':'Some error in logout try again'});
+		res.status(400).send({ 'error': 'Some error in logout try again' });
 	}
 };
 
@@ -128,9 +125,9 @@ module.exports.logout = (req, res, next) => {
  *  Middleware for checking if user is logged in or not
  */
 module.exports.checkUser = (req, res, next) => {
-  if (req.session.user && req.session.user._id) {
-    next();
-  } else {
+	if (req.session.user && req.session.user._id) {
+		next();
+	} else {
 		res.status(400).send('Login first');
-  }
+	}
 };
